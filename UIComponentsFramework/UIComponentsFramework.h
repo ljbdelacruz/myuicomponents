b6c86db5d46e8dd8d8991e5d90fcf93f4cc32a8c
Lainel John Dela Cruz <laineljohndelacruz@lainels-macbook-pro.local>
2019-05-19 09:04:08 +0800

    Initial Commit

---
+++ UIComponentsFramework/UIComponentsFramework.h
@@ -0,0 +1,19 @@
+//
+//  UIComponentsFramework.h
+//  UIComponentsFramework
+//
+//  Created by Lainel John Dela Cruz on 5/19/19.
+//  Copyright © 2019 Lainel John Dela Cruz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UIComponentsFramework.
+FOUNDATION_EXPORT double UIComponentsFrameworkVersionNumber;
+
+//! Project version string for UIComponentsFramework.
+FOUNDATION_EXPORT const unsigned char UIComponentsFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UIComponentsFramework/PublicHeader.h>
+
+
