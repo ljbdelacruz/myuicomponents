d637a21512c05620f21c623cb2f926e86f0aa0ec
Lainel John Dela Cruz <laineljohndelacruz@Lainels-MacBook-Pro.local>
2019-05-24 23:25:22 +0800

    added card view ui

---
+++ UIComponentsFramework/Views/CardDisplay/card1view/card1View.swift
@@ -0,0 +1,57 @@
+//
+//  card1View.swift
+//  UIComponentsFramework
+//
+//  Created by Lainel John Dela Cruz on 5/24/19.
+//  Copyright © 2019 Lainel John Dela Cruz. All rights reserved.
+//
+
+import UIKit
+
+public class card1View: UIView {
+    public let xibname:String="card1View";
+    
+    @IBOutlet var containerView: UIView!
+    @IBOutlet weak var cardImage: UIImageView!
+    @IBOutlet weak var cardName: UILabel!
+    @IBOutlet weak var cardNumber: UILabel!
+    public var eventOnClick: (() -> Void)?{
+        didSet{
+            self.setupTapRecognizer();
+        }
+        
+    }
+    
+    
+    override public init(frame: CGRect){
+        super.init(frame: frame)
+        commonInit()
+    }
+    required public init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        commonInit()
+    }
+    public func commonInit() {
+        Bundle.main.loadNibNamed(xibname, owner: self, options: nil)
+        contentview.fixInView(self)
+        self.mylabel.text="";
+        self.mycontent.text="";
+    }
+    
+    
+}
+//MARK: card recog
+extension card1View{
+    public func getRecog()->UITapGestureRecognizer{
+        let recog = UITapGestureRecognizer(target: self, action: #selector(onClick))
+        return recog;
+    }
+    public func setupTapRecognizer(){
+        containerView.addGestureRecognizer(self.getRecog());
+        
+    }
+    @objc
+    func onClick(){
+        eventOnClick?();
+    }
+}
