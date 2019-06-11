6c08c06b4df0bae6dd2aa6795bd054014feae5fa
Lainel John Dela Cruz <ljbdelacruz123@gmail.com>
2019-06-11 15:21:59 +0800

    added reusable ui and script enhancements

--- UIComponentsFramework/Views/Ads/AdsTemplate1/AdsTemplate1.swift
+++ UIComponentsFramework/Views/Ads/AdsTemplate1/AdsTemplate1.swift
@@ -1,25 +1,24 @@
 //
-//  Content1View.swift
+//  AdsTemplate1.swift
 //  UIComponentsFramework
 //
-//  Created by devops on 21/05/2019.
+//  Created by devops on 11/06/2019.
 //  Copyright © 2019 Lainel John Dela Cruz. All rights reserved.
 //
 
 import UIKit
 
-public class Content1View: UIView {
-    public let xibname:String="Content1View";
+public class AdsTemplate1: UIView {
+    public let xibname:String="AdsTemplate1";
     @IBOutlet public var contentview: UIView!
     @IBOutlet public weak var myimage: UIImageView!
-    @IBOutlet public weak var mylabel: UILabel!
-    @IBOutlet public weak var mycontent: UILabel!
+    @IBOutlet public weak var mytitle: UILabel!
+    @IBOutlet public weak var mySubDesc: UILabel!
     public var eventOnClick: (() -> Void)?{
         didSet{
             contentview.setUIRecognizer(selector: #selector(onClick))
         }
     }
-    
     
     override public init(frame: CGRect){
         super.init(frame: frame)
@@ -32,16 +31,17 @@ public class Content1View: UIView {
     public func commonInit() {
         Bundle.main.loadNibNamed(xibname, owner: self, options: nil)
         contentview.fixInView(self)
-        self.mylabel.text="";
-        self.mycontent.text="";
-    }
-    public func unsetImage(){
-        mylabel.translatesAutoresizingMaskIntoConstraints = false
-        self.trailingAnchor.constraint(equalTo: contentview.trailingAnchor, constant: 1);
+        //setting rounded corners
+        contentview.setupRadius(bgColor: UIColor.init(white: 0, alpha: 0), radius: 10);
+        //setting shadow 
+        contentview.setMyShadow(radius: 10, width: 1, height: 1, opacity: 1);
+        //setting image top rounded corners
+        myimage.roundCorners(corners: [.topLeft, .topRight], radius: 10);
+        
     }
 }
 //MARK: setting up recognizer
-extension Content1View{
+extension AdsTemplate1{
     @objc
     func onClick(){
         eventOnClick?();
