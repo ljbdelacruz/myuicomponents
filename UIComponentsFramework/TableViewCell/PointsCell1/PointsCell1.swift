2f7162a59f5353d42f6d053500872ac547f3a8e8
Lainel John Dela Cruz <ljbdelacruz123@gmail.com>
2019-06-11 17:00:39 +0800

    added points cell

---
+++ UIComponentsFramework/TableViewCell/PointsCell1/PointsCell1.swift
@@ -0,0 +1,26 @@
+//
+//  PointsCell1.swift
+//  UIComponentsFramework
+//
+//  Created by devops on 11/06/2019.
+//  Copyright © 2019 Lainel John Dela Cruz. All rights reserved.
+//
+
+import UIKit
+
+public class PointsCell1: UITableViewCell {
+    public static let identifier:String="PointsCell1";
+    public static let nibname:String="PointsCell1";
+    @IBOutlet weak var monthLabel: UILabel!
+    @IBOutlet weak var points: UILabel!
+    @IBOutlet weak var subDesc: UILabel!
+    
+    
+    override public func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    override public func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+
+}
